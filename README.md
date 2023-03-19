d301e9ade91ad5992186fd8d80be2f3d5bafff68
carrel <dvquentincarrel@gmail.com>
2023-03-19 02:01:19 +0100

    [MA] README, feh_help, setup
    [M] feh_help now uses FOTM_TERM
        README has more line breaks
    [A] setup checks for requirements, handles executables

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ## Purpose
 
-This repo features all the scripts and configs related to my use of the *feh*
+This repo features all the scripts and configs related to my use of the `feh`
 image viewer.
 
 ## Setting up
-Keybindings are in the **keys** file. A symlink named `~/.config/feh/keys`
+Keybindings are in the **keys** file.  
+A symlink named `~/.config/feh/keys`
 should point to it.
 
-Default options and further configurations are inside the **themes** file.
+Default options and further configurations are inside the **themes** file.  
 A symlink named `~/.config/feh/themes` should point to it.
--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-# Setup script, handles symlink creation
-feh_config="~/.config/feh"
-ln -si $PWD/keys $feh_config/keys
-ln -si $PWD/themes $feh_config/themes
+# Setup script, checks for dependencies, handles symlink creation
+
+command -v identify >/dev/null || echo "Identify not found. Can be found in package 'imagemagick'"
+command -v notify-send >/dev/null || echo "Notify-send not found. Can be found in package 'libnotify'"
+
+lbin="$HOME/.local/bin"
+mkdir -p "$HOME/.local/bin"
+ln -si "$PWD/feh_del"   "$lbin/feh_del"
+ln -si "$PWD/feh_info"  "$lbin/feh_info" 
+ln -si "$PWD/feh_help"  "$lbin/feh_help"
+ln -si "$PWD/dyn_papes" "$lbin/dyn_papes"
+ln -si "$PWD/rand_dbg"  "$lbin/rand_dbg"
+
+feh_config="$HOME/.config/feh"
+ln -si "$PWD/keys"   "$feh_config/keys"
+ln -si "$PWD/themes" "$feh_config/themes"
