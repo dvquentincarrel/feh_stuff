f95b4308a8c0385a1bdf46a3beb5437930fc885e
carrel <dvquentincarrel@gmail.com>
2023-03-21 23:02:36 +0100

    [AR] setup: feth, links made in a loop

--- setup.sh
+++ setup.sh
@@ -6,12 +6,11 @@ command -v notify-send >/dev/null || echo "Notify-send not found. Can be found i
 
 lbin="$HOME/.local/bin"
 mkdir -p "$HOME/.local/bin"
-ln -si "$PWD/feh"       "$lbin/feh"
-ln -si "$PWD/feh_del"   "$lbin/feh_del"
-ln -si "$PWD/feh_info"  "$lbin/feh_info" 
-ln -si "$PWD/feh_help"  "$lbin/feh_help"
-ln -si "$PWD/dyn_papes" "$lbin/dyn_papes"
-ln -si "$PWD/rand_dbg"  "$lbin/rand_dbg"
+for executable in \
+        feh feth feh_pl feh_del \
+        feh_info feh_help dyn_papes rand_dbg; do
+    ln -si "$PWD/$executable" "$lbin/$executable"
+done
 
 feh_config="$HOME/.config/feh"
 ln -si "$PWD/keys"   "$feh_config/keys"
